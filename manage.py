5beac244d375098070bf65479dfdae3bc064f139
Lennox Omondi <omondilenox@outlook.com>
2021-03-14 22:48:11 +0300

    Removed unused values + restructured the script

--- manage.py
+++ manage.py
@@ -1,7 +1,6 @@
 from app import create_app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
-from app.models import Project, Category, Image
 
 app = create_app('development')
 
@@ -12,7 +11,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict(app=app, db=db, project=project, category=category, image=image)
+    return dict(app=app, db=db)
 
 
 if __name__ == '__main__':
