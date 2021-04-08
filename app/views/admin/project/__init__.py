c36f3d8ca4e3d16e7a6c364e549eb634e1170f69
Lennox Omondi <omondilenox@outlook.com>
2021-04-08 22:57:30 +0300

    finished project crud

--- app/views/admin/project/__init__.py
+++ app/views/admin/project/__init__.py
@@ -14,47 +14,35 @@ class ProjectView(FlaskView):
     decorators = [login_required]
 
     def index(self):
-        projects = Project.query.filter_by(type="combined").all()
+        projects = Project.query.all()
         return render_template('admin/project/index.html', projects=projects)
 
+    @route('/create', methods=('POST', 'GET',))
     def create(self):
         form = ProjectForm()
-        return render_template('admin/project/create.html', form=form)
-
-    @route('/', methods=('POST',))
-    def store(self):
-        form = ProjectForm(request.form)
         if form.validate_on_submit():
             project = Project(title=form.title.data, description=form.description.data)
             project.save()
             flash("Project was created successfully", 'success')
             return redirect(url_for("admin.ProjectView:index"))
-        else:
-            return redirect(url_for("admin.ProjectView:create"))
+        return render_template('admin/project/create.html', form=form)
 
     @route('/<project_id>', methods=('GET',))
     def show(self, project_id):
         project = get_project(project_id)
         return render_template('admin/project/show.html', project=project)
 
-    @route('/<project_id>/edit', methods=('GET',))
-    def edit(self, project_id):
-        project = get_project(project_id)
-        form = ProjectForm(title=project.title, description=project.description)
-        return render_template('admin/project/edit.html', form=form, project_id=project_id)
-
-    @route('/<project_id>/update', methods=('POST',))
+    @route('/<project_id>/update', methods=('GET', 'POST',))
     def update(self, project_id):
         project = get_project(project_id)
-        form = ProjectForm(request.form)
+        form = ProjectForm(title=project.title, description=project.description)
         if form.validate_on_submit():
             project.title = form.title.data
             project.description = form.description.data
             project.save()
             flash("Project was updated successfully", "success")
             return redirect(url_for("admin.ProjectView:index"))
-        else:
-            return redirect(request.url)
+        return render_template('admin/project/edit.html', form=form, project_id=project_id)
 
     @route('<project_id>/delete', methods=('POST',))
     def delete(self, project_id):
