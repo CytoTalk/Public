47a8652369b4df80109b50c0f10532c9d6f3ad25
Lennox Omondi <omondilenox@outlook.com>
2021-04-08 21:40:04 +0300

    Database frontend module working

--- app/views/database.py
+++ app/views/database.py
@@ -4,19 +4,18 @@ from flask import send_from_directory, current_app
 from flask import render_template
 from flask_classful import FlaskView, route
 from app.models.Database import DatabaseCategory as Category
-from app.models.Database import Image
-from app.models.Project import Project
+from app.models.Database import Image,Database
 
 
 class DatabaseView(FlaskView):
     def index(self):
-        databases = Project.query.all()
+        databases = Database.query.all()
         print(databases)
         return render_template('front/database/index.html', databases=databases)
 
     @route('/<database_id>', methods=('GET',))
     def show(self, database_id):
-        database = Project.query.filter_by(id=database_id).first_or_404()
+        database = Database.query.filter_by(id=database_id).first_or_404()
         categories = Category.query.filter_by(database_id=database_id).all()
         return render_template('front/database/show.html', database=database, categories=categories)
 
@@ -28,5 +27,5 @@ class DatabaseView(FlaskView):
 
     @route('/database/<database_id>/images', methods=('GET',))
     def show(self, database_id):
-        database = Project.query.filter_by(id=database_id).first_or_404()
+        database = Database.query.filter_by(id=database_id).first_or_404()
         return render_template('front/database/show.html', database=database)
