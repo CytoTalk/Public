8eb7e3ab15b352c59e854e3e161dd09429643b61
Lennox Omondi <omondilenox@outlook.com>
2021-04-10 10:35:41 +0300

    added change password func + updated create_admin func

--- manage.py
+++ manage.py
@@ -6,9 +6,9 @@ from flask_script import Manager
 
 from app import create_app, db
 from app.auth.security import hash_password
-from app.models.Project import Project,SubProject,ImageStore,ImageCategory
-from app.models.Excel import ExcelRecord,ExcelColumn
-from app.models.Database import DatabaseCategory,Image,Database
+from app.models.Project import Project, SubProject, ImageStore, ImageCategory
+from app.models.Excel import ExcelRecord, ExcelColumn
+from app.models.Database import DatabaseCategory, Image, Database
 from app.models.User import User
 
 app = create_app()
@@ -26,6 +26,12 @@ def make_shell_context():
 @manager.command
 def create_admin():
     """Creates the admin user."""
+    user = User.query.filter_by(is_admin=True).first()
+    if user:
+        print(f"Admin Account already exist. The username is '{user.email}'. To change password, run python manage.py "
+              f"change_admin_password")
+        exit()
+
     email = input("Email Address: ")
     password = " "
     confirm_password = ""
@@ -37,13 +43,35 @@ def create_admin():
 
     user = User(
         email=email,
-        password=hash_password(password),
+        password=password,
+        # password=hash_password(password),
         is_admin=True,
+        is_active=True,
         first_name="Super",
         last_name="Admin",
         email_verified_at=datetime.datetime.now())
     user.create()
     print("Account was created successfully.")
+
+
+@manager.command
+def change_admin_password():
+    """Changes Super admin password."""
+    password = " "
+    confirm_password = ""
+    while password != confirm_password:
+        password = getpass("New password: ")
+        confirm_password = getpass("Confirm Password: ")
+        if password and confirm_password and confirm_password != password:
+            print("Password do not match")
+
+    user = User.query.filter_by(is_admin=True).first()
+    if user:
+        user.password = password
+        user.save()
+        print("Password was updated successfully.")
+    else:
+        "No admin account found, please run python manage.py create_admin to add an admin account"
 
 
 if __name__ == '__main__':
